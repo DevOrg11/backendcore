404c377ea7268ea0e342c90f63be125c53e8ab1f
Aslam Marikkar <amarikkar@moqdigital.com.au>
2022-10-24 03:41:17 +1000

    test

--- Clean.Api/Program.cs
+++ Clean.Api/Program.cs
@@ -7,21 +7,21 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IItemRepository, ItemRepository>();
 builder.Services.AddSingleton<IItemService, ItemService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-/*
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-*/
+
 
 app.UseHttpsRedirection();
 
